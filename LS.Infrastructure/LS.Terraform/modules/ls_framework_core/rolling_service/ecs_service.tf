e6ecfda483511e48e58cba7fa247232da4f6e3c3
Bouba <sogoba3@gmail.com>
2026-06-22 16:28:14 -0400

    Fix ECS deployment and worker service configuration

--- LS.Infrastructure/LS.Terraform/modules/ls_framework_core/rolling_service/ecs_service.tf
+++ LS.Infrastructure/LS.Terraform/modules/ls_framework_core/rolling_service/ecs_service.tf
@@ -18,7 +18,7 @@ resource "aws_ecs_service" "ls_framework_ecs_service_name" {
   service_registries {
     registry_arn = var.ls_framework_service_discovery_arn
     container_name = var.service_name
-    container_port = var.container_port
+    # container_port = var.container_port
   }
   # ECS NETWORKING
   network_configuration {
